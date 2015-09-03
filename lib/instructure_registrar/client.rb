b497d39ed3df0133e4a95180062cfcfccec21672
CoralineAda <coraline@idolhands.com>
2015-09-03 12:48:51 -0500

    Debugging

--- lib/instructure_registrar/client.rb
+++ lib/instructure_registrar/client.rb
@@ -27,8 +27,8 @@ module InstructureRegistrar
     def register
       return unless server_available
       InstructureRegistrar.config.service_config.keys.each do |key|
-        p "InstructureRegistrar setting #{InstructureRegistrar.config.service_name}/#{key} on #{InstructureRegistrar.config.registry_host}..."
-        client.set(
+        p "InstructureRegistrar setting #{InstructureRegistrar.config.service_name}/#{key}"
+        p client.set(
           "/#{InstructureRegistrar.config.service_name}/#{key}",
           value: InstructureRegistrar.config.service_config[key]
         )
